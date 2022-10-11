706a73b65865f223e212ec0f21a97278d3ffe54c
hugo <hugo0703@wanadoo.fr>
2022-10-11 13:47:09 +0200

    hjlhlj

--- js/jquery-3.6.1.js
+++ js/jquery-3.6.1.js
@@ -10384,7 +10384,7 @@ jQuery.parseHTML = function( data, context, keepScripts ) {
  * Load a url into a page
  */
 
-/*
+
 jQuery.fn.load = function( url, params, callback ) {
 	var selector, type, response,
 		self = this,
@@ -10445,7 +10445,7 @@ jQuery.fn.load = function( url, params, callback ) {
 	return this;
 };
 
-*/
+
 
 
 jQuery.expr.pseudos.animated = function( elem ) {
